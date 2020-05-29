7a24780dde8ac280d25afc88a63bbf1677c67d35
Govindarajan V <govindarajanv@gmail.com>
2020-05-29 17:55:04 +0530

    Moved import to the top

--- blueprint/app.py
+++ blueprint/app.py
@@ -1,17 +1,17 @@
+from flask import Flask
+
 class Blueprint:
 
     @staticmethod
     def run():
         print("Hello World...")
 
-from flask import Flask
-app = Flask(__name__)
 
+app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Hello World!"
-
 
 @app.route('/<name>')
 def hello_name(name):
